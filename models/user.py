9160b2525e4ab4bd4ca141e317698c63e3d85213
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-03-02 00:46:36 +0400

    Create user.py

---
+++ models/user.py
@@ -0,0 +1,9 @@
+from models.base_model import BaseModel
+"""This module define for a User class"""
+
+class User(BaseModel):
+    """User class"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
