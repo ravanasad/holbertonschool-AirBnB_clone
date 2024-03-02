08c73709951d09280302c976eae032b127d4e4c2
Kananmrzzd <kananmirzazada@gmail.com>
2024-03-02 17:47:40 +0400

    a

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines the Amenity class."""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Amenity class
+    """
+
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines the State class."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    State class
+    """
+
+    name = ""
