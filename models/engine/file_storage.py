94f1d9d8089cde7bbcbd63ac253a3d746d416924
Kananmrzzd <kananmirzazada@gmail.com>
2024-03-03 13:36:37 +0400

    fixed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,11 @@
 import json
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 """This module define for a FileStorage class"""
 
