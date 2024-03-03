68bf520182d15403cede5e83f1217fcfcd8ada62
Kananmrzzd <146704930+Kananmrzzd@users.noreply.github.com>
2024-03-03 13:57:18 +0400

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -30,7 +30,7 @@ class BaseModel:
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-    
+
     def save(self):
         from models import storage
         """updates the public instance attribute updated_at """
